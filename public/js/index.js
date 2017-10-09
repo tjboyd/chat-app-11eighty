c4b4ed5d0e1ea79f322a36d60908e3d0a43a1337
11eighty <tjboyd20@gmail.com>
2017-10-09 16:20:06 -0500

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'tom@example.com',
-    text: 'Yup, got it!'
-  });
 });
 
 socket.on('disconnect', function() {
