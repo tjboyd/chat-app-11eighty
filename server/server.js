811c0e6988acc26684a9c08180e93311d4dcaf7c
11eighty <tjboyd20@gmail.com>
2017-10-11 04:21:04 -0500

    added send location message

--- server/server.js
+++ server/server.js
@@ -31,6 +31,10 @@ io.on('connection', (socket) => {
     // });
   });
 
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newMessage', generateMessage('Admin', `${coords.latitude}, ${coords.longitude}`));
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
