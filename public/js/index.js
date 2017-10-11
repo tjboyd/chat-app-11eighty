c9b967d4e368a43cbe60881620886dd4871a70b8
11eighty <tjboyd20@gmail.com>
2017-10-11 07:36:38 -0500

    added mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -1,34 +1,38 @@
 var socket = io();
 
-socket.on('connect', function() {
+socket.on('connect', function () {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', function() {
-  console.log('Disconnected from server.');
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message) {
+socket.on('newMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
-socket.on('newLocationMessage', function(message) {
+socket.on('newLocationMessage', function (message) {
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>');
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
-jQuery('#message-form').on('submit', function(e) {
+jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
   var messageTextbox = jQuery('[name=message]');
@@ -36,13 +40,12 @@ jQuery('#message-form').on('submit', function(e) {
   socket.emit('createMessage', {
     from: 'User',
     text: messageTextbox.val()
-  }, function() {
+  }, function () {
     messageTextbox.val('')
   });
 });
 
 var locationButton = jQuery('#send-location');
-
 locationButton.on('click', function () {
   if (!navigator.geolocation) {
     return alert('Geolocation not supported by your browser.');
